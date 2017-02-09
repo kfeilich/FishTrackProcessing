2c1590f9df39624b71351fa471b2ae5f6443d851
Kara <kfeilich@gmail.com>
2017-02-09 10:28:53 -0500

    Updated to incorporate smoothing and plotting; multiple plot figure showing 3d position, streamwise velocity, and streamwise acceleration (1 multiplot figure per trial). Note, velocity and accel plots show (-) values, so +  indicated forward movement into the flow

--- plot_track.py
+++ plot_track.py
@@ -5,39 +5,17 @@ Created on Mon Feb  6 14:24:53 2017
 @author: Kara
 """
 from mpl_toolkits.mplot3d import Axes3D
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy
-from sympy import *
-from scipy.signal import savgol_filter
 
 
-#import points
-data = np.genfromtxt("E:/Digitized tracks/BTrout1_S08_tracks_xyzmerge.csv",delimiter=',',skip_header=1)
 
+scaled_time = (data.index.values - data.index.values.min()) / data.index.ptp()  # scale time for colormap
+colors = plt.cm.cubehelix(scaled_time)  # pull from colormap (here cubehelix)
 
-#extract useful variables
-
-pt1x = data[:,1] #point 1 x-component
-pt1y = data[:,2] #point 1 y-component
-pt1z = data[:,0] #point 1 z-component
-
-pt2x = data[:,4] #point 2 x-component
-pt2y = data[:,5] #point 2 y-component
-pt2z = data[:,3] #point 2 z-component
-
-#turn positions into cm from pixels
-
-
-#generate time array
-time = np.linspace(0,len(pt1x)*0.002,num=len(pt1x),endpoint=False)
-dt = 0.002
-
-#plot Raw 3d position, color by time
-scaled_time = (time-time.min())/time.ptp() #scale time for colormap
-colors = plt.cm.cubehelix(scaled_time) #pull from colormap (here cubehelix)
-
-#Raw Plot Pt 1
+#Raw Plot Pt 1, 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter3D(xs=pt1x, ys=pt1y, zs=pt1z,zdir='z', s=3,c=colors, marker='o', edgecolor='none')#Scatter plot
@@ -45,9 +23,10 @@ ax.scatter3D(xs=pt1x, ys=pt1y, zs=pt1z,zdir='z', s=3,c=colors, marker='o', edgec
 #ax.set_ylim3d(0,1000)
 #ax.set_zlim3d(0,1000)
 ax.autoscale(enable=True, tight=None)
+fig.colorbar(cubehelix, shrink=0.5, aspect=10)
 plt.show()
 
-#Raw Plot Pt 2
+#Raw Plot Pt 2, 3D
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111, projection='3d')
 ax2.scatter3D(xs=pt2x, ys=pt2y, zs=pt2z, zdir='z', s=3,c=colors, marker='o', edgecolor='none')#Scatter plot
@@ -57,18 +36,7 @@ ax2.scatter3D(xs=pt2x, ys=pt2y, zs=pt2z, zdir='z', s=3,c=colors, marker='o', edg
 ax2.autoscale(enable=True, tight=None)
 plt.show()
 
-
-#smooth data using savitzky golay
-pt1x_smth = scipy.signal.savgol_filter(pt1x, window_length = 91, polyorder = 2)
-pt1y_smth = scipy.signal.savgol_filter(pt1y, window_length = 91, polyorder = 2)
-pt1z_smth = scipy.signal.savgol_filter(pt1z, window_length = 91, polyorder = 2)
-
-pt2x_smth = scipy.signal.savgol_filter(pt2x, window_length = 91, polyorder = 2)
-pt2y_smth = scipy.signal.savgol_filter(pt2y, window_length = 91, polyorder = 2)
-pt2z_smth = scipy.signal.savgol_filter(pt2z, window_length = 91, polyorder = 2)
-
-
-#plot pt1 smooth data 
+#Smoothed Data Pt 1, 3D
 fig3 = plt.figure()
 ax3 = fig3.add_subplot(111, projection='3d')
 ax3.scatter3D(xs=pt1x_smth, ys=pt1y_smth, zs=pt1z_smth,zdir='z', s=3,c=colors, marker='o', edgecolor='none')#Scatter plot
@@ -78,7 +46,7 @@ ax3.scatter3D(xs=pt1x_smth, ys=pt1y_smth, zs=pt1z_smth,zdir='z', s=3,c=colors, m
 ax3.autoscale(enable=True, tight=None)
 plt.show()
 
-#plot pt2 smooth data 
+#Smoothed Dara Pt 2, 3D
 fig4 = plt.figure()
 ax4 = fig4.add_subplot(111, projection='3d')
 ax4.scatter3D(xs=pt2x_smth, ys=pt2y_smth, zs=pt2z_smth,zdir='z', s=3,c=colors, marker='o', edgecolor='none')#Scatter plot
@@ -102,24 +70,6 @@ time_v = time[:len(time)-1]
 #PLOT WHATEVS
 plt.scatter(x = time_v, y=-pt1x_v)
 plt.show()
-
-#Smooth Vel
-pt1x_v_smth = scipy.signal.savgol_filter(pt1x_v, window_length = 51, polyorder = 2)
-pt1y_v_smth = scipy.signal.savgol_filter(pt1y_v, window_length = 51, polyorder = 2)
-pt1z_v_smth = scipy.signal.savgol_filter(pt1z_v, window_length = 51, polyorder = 2)
-
-pt2x_v_smth = scipy.signal.savgol_filter(pt2x_v, window_length = 51, polyorder = 2)
-pt2y_v_smth = scipy.signal.savgol_filter(pt2y_v, window_length = 51, polyorder = 2)
-pt2z_v_smth = scipy.signal.savgol_filter(pt2z_v, window_length = 51, polyorder = 2)
-
-#DERIVATIVES -- ACCEL
-pt1x_a = np.ediff1d(pt1x_v_smth)
-pt1y_a = np.ediff1d(pt1y_v_smth)
-pt1z_a = np.ediff1d(pt1z_v_smth)
-
-pt2x_a = np.ediff1d(pt2x_v_smth)
-pt2y_a = np.ediff1d(pt2y_v_smth)
-pt2z_a = np.ediff1d(pt2z_v_smth)
 
 time_a = time_v[:len(time_v)-1]
 
--- process_points_pandas.py
+++ process_points_pandas.py
@@ -6,19 +6,27 @@ import pandas as pd
 import numpy as np
 import scipy
 from scipy.signal import savgol_filter #for smoothing data
+from mpl_toolkits.mplot3d import Axes3D
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
-root = tk.Tk()  
+
+root = tk.Tk()
 root.withdraw()
+######################################################################################################################
+#Data Extraction
+######################################################################################################################
 
 #User input
-folder = filedialog.askdirectory()  #Ask user for directory
-framerate = float(input('Enter frame rate in frames per second:')) #Probably 500; Note assumes all videos at same framerate
-
+#folder = filedialog.askdirectory()  #Ask user for directory
+#framerate = float(input('Enter frame rate in frames per second:')) #Probably 500; Note assumes all videos at same framerate
+folder = 'E:/Digitized Tracks'
+framerate = 500
          
-#Initialize a list of dictionaries to contain each trial's data         
+#Initialize a list of dictionaries to contain each trial's data
 tracklist =  {}
 count = 0 # Initialize the count 
-
 
 for filename in os.listdir(folder): #For all of the files in the directory
     if filename.endswith("xypts.csv"): #that end with 'xypts.csv'
@@ -38,23 +46,32 @@ for filename in os.listdir(folder): #For all of the files in the directory
         df = df.set_index(['time'])
        
         # Smooth position data using savitzky golay
-        df['pt1x_smth'] = scipy.signal.savgol_filter(df['pt1x'], window_length = 91, polyorder = 2)
-        df['pt1y_smth'] = scipy.signal.savgol_filter(df['pt1y'], window_length = 91, polyorder = 2)
-        df['pt1z_smth'] = scipy.signal.savgol_filter(df['pt1z'], window_length = 91, polyorder = 2)
+        df['pt1x_smth'] = scipy.signal.savgol_filter(df['pt1x'], window_length = 121, polyorder = 3)
+        df['pt1y_smth'] = scipy.signal.savgol_filter(df['pt1y'], window_length = 121, polyorder = 3)
+        df['pt1z_smth'] = scipy.signal.savgol_filter(df['pt1z'], window_length = 121, polyorder = 3)
         
-        df['pt2x_smth'] = scipy.signal.savgol_filter(df['pt2x'], window_length = 91, polyorder = 2)
-        df['pt2y_smth'] = scipy.signal.savgol_filter(df['pt2y'], window_length = 91, polyorder = 2)
-        df['pt2z_smth'] = scipy.signal.savgol_filter(df['pt2z'], window_length = 91, polyorder = 2)
+        df['pt2x_smth'] = scipy.signal.savgol_filter(df['pt2x'], window_length = 121, polyorder = 3)
+        df['pt2y_smth'] = scipy.signal.savgol_filter(df['pt2y'], window_length = 121, polyorder = 3)
+        df['pt2z_smth'] = scipy.signal.savgol_filter(df['pt2z'], window_length = 121, polyorder = 3)
         
         # Calculate First Discrete Differences (Velocity)     
         cols_to_use1 = ['pt1x_smth','pt1y_smth','pt1z_smth','pt2x_smth','pt2y_smth','pt2z_smth']
         df2 = df.loc[:,cols_to_use1].diff()
-        df2 = df2.rename(columns = {'pt1z_smth':'pt1z_v', 'pt1x_smth':'pt1x_v', 'pt1y_smth':'pt1y_v', 'pt2z_smth':'pt2z_v','pt2x_smth':'pt2x_v', 'pt2y_smth':'pt2y_v'})    
+        df2 = df2.rename(columns = {'pt1z_smth':'pt1z_v', 'pt1x_smth':'pt1x_v', 'pt1y_smth':'pt1y_v', 'pt2z_smth':'pt2z_v','pt2x_smth':'pt2x_v', 'pt2y_smth':'pt2y_v'})
+
+        # Smooth velocity data using savitzky golay
+        df2['pt1x_v_smth'] = scipy.signal.savgol_filter(df2['pt1x_v'], window_length=121, polyorder=3)
+        df2['pt1y_v_smth'] = scipy.signal.savgol_filter(df2['pt1y_v'], window_length=121, polyorder=3)
+        df2['pt1z_v_smth'] = scipy.signal.savgol_filter(df2['pt1z_v'], window_length=121, polyorder=3)
+
+        df2['pt2x_v_smth'] = scipy.signal.savgol_filter(df2['pt2x_v'], window_length=121, polyorder=3)
+        df2['pt2y_v_smth'] = scipy.signal.savgol_filter(df2['pt2y_v'], window_length=121, polyorder=3)
+        df2['pt2z_v_smth'] = scipy.signal.savgol_filter(df2['pt2z_v'], window_length=121, polyorder=3)
         
         # Calculate Second Discrete Differences (Acceleration)   
-        cols_to_use2 = ['pt1x_v','pt1y_v','pt1z_v','pt2x_v','pt2y_v','pt2z_v']
+        cols_to_use2 = ['pt1x_v_smth','pt1y_v_smth','pt1z_v_smth','pt2x_v_smth','pt2y_v_smth','pt2z_v_smth']
         df3 = df2.loc[:,cols_to_use2].diff()
-        df3 = df3.rename(columns = {'pt1z_v':'pt1z_a', 'pt1x_v':'pt1x_a', 'pt1y_v':'pt1y_a', 'pt2z_v':'pt2z_a','pt2x_v':'pt2x_a', 'pt2y_v':'pt2y_a'})
+        df3 = df3.rename(columns = {'pt1z_v_smth': 'pt1z_a', 'pt1x_v_smth': 'pt1x_a', 'pt1y_v_smth': 'pt1y_a', 'pt2z_v_smth': 'pt2z_a','pt2x_v_smth': 'pt2x_a', 'pt2y_v_smth': 'pt2y_a'})
 
         # Merge all this shit
         df = df.merge(df2, how = 'outer', left_index=True, right_index=True)
@@ -65,3 +82,100 @@ for filename in os.listdir(folder): #For all of the files in the directory
         
         #Advance the count
         count = count + 1
+
+######################################################################################################################
+##Plotting
+######################################################################################################################
+for trial in tracklist:# Iterates over all avalable trials
+
+    scaled_time = (tracklist[trial]['data'].index.values - tracklist[trial]['data'].index.values.min()) / tracklist[trial]['data'].index.values.ptp()  # scale time for colormap
+    colors = plt.cm.cubehelix(scaled_time)  # pull from colormap (here cubehelix)
+
+    #Raw Plot Pt 1, 3D
+    fig = plt.figure()
+    fig.suptitle(tracklist[trial]['sequence'])
+    ax = fig.add_subplot(4,2,1, projection='3d')
+    ax.set_title('Pt 1 Raw Position')
+    p = ax.scatter3D(xs=tracklist[trial]['data']['pt1x'], ys=tracklist[trial]['data']['pt1y'], zs=tracklist[trial]['data']['pt1z'], zdir='z', s=3, c=colors, marker='o', edgecolor='none') #3D Scatter plot
+    #ax.set_xlim3d(0,1000)
+    #ax.set_ylim3d(0,1000)
+    #ax.set_zlim3d(0,1000)
+    ax.autoscale(enable=True, tight=None)
+    ax.set_xlabel('X position')
+    ax.set_ylabel('Y position')
+    ax.set_zlabel('Z position')
+    m = cm.ScalarMappable(cmap = cm.cubehelix)
+    m.set_array(tracklist[trial]['data'].index.values)
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    #plt.show()
+
+    #Raw Plot Pt 2, 3D
+    ax = fig.add_subplot(4,2,2, projection='3d')
+    ax.set_title('Pt 2 Raw Position')
+    p2 = ax.scatter3D(xs=tracklist[trial]['data']['pt2x'], ys=tracklist[trial]['data']['pt2y'], zs=tracklist[trial]['data']['pt2z'], zdir='z', s=3, c=colors, marker='o', edgecolor='none') #3D Scatter plot
+    # #ax2.set_xlim3d(0,1000)
+    # #ax2.set_ylim3d(0,1000)
+    # #ax2.set_zlim3d(0,1000)
+    ax.autoscale(enable=True, tight=None)
+    ax.set_xlabel('X position')
+    ax.set_ylabel('Y position')
+    ax.set_zlabel('Z position')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    #plt.show()
+
+    #Smoothed Data Pt 1, 3D
+    ax = fig.add_subplot(4, 2, 3, projection='3d')
+    ax.set_title('Pt 1 Smoothed Position')
+    p3 = ax.scatter3D(xs=tracklist[trial]['data']['pt1x_smth'], ys=tracklist[trial]['data']['pt1y_smth'], zs=tracklist[trial]['data']['pt1z_smth'], zdir='z', s=3, c=colors, marker='o', edgecolor='none')#Scatter plot
+    # #ax3.set_xlim3d(0,1000)
+    # #ax3.set_ylim3d(0,1000)
+    # #ax3.set_zlim3d(0,1000)
+    ax.autoscale(enable=True, tight=None)
+    ax.set_xlabel('X position')
+    ax.set_ylabel('Y position')
+    ax.set_zlabel('Z position')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    #plt.show()
+
+    #Smoothed Data Pt 2, 3D
+    ax = fig.add_subplot(4,2,4, projection='3d')
+    ax.set_title('Pt 2 Smoothed Position')
+    p4 = ax.scatter3D(xs=tracklist[trial]['data']['pt2x_smth'], ys=tracklist[trial]['data']['pt2y_smth'], zs=tracklist[trial]['data']['pt2z_smth'], zdir='z', s=3, c=colors, marker='o', edgecolor='none')#Scatter plot
+    # #ax4.set_xlim3d(0,1000)
+    # #ax4.set_ylim3d(0,1000)
+    # #ax4.set_zlim3d(0,1000)
+    ax.autoscale(enable=True, tight=None)
+    ax.set_xlabel('X position')
+    ax.set_ylabel('Y position')
+    ax.set_zlabel('Z position')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    #plt.show()
+
+    #Streamwise Velocity Pt 1
+    ax = fig.add_subplot(4,2,5)
+    ax.set_title('Pt 1 Streamwise Velocity')
+    plt.scatter(x = tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt1x_v_smth'], c=colors, edgecolor='none')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    #plt.show()
+
+    #Streamwise Velocity Pt 2
+    ax = fig.add_subplot(4,2,6)
+    ax.set_title('Pt 2 Streamwise Velocity')
+    plt.scatter(x = tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt2x_v_smth'], c=colors, edgecolor='none')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    #plt.show()
+
+    # Streamwise Accel Pt 1
+    ax = fig.add_subplot(4, 2, 7)
+    ax.set_title('Pt 1 Streamwise Acceleration')
+    plt.scatter(x=tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt1x_a'], c=colors, edgecolor='none')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    # plt.show()
+
+    # Streamwise Accel Pt 2
+    ax = fig.add_subplot(4, 2, 8)
+    ax.set_title('Pt 2 Streamwise Acceleration')
+    plt.scatter(x=tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt2x_a'], c=colors, edgecolor='none')
+    plt.colorbar(m, shrink=0.5, aspect=10)
+    plt.show()
+
