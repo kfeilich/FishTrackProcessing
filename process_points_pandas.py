aef0562ff82f40bf2c3ddcad86d4a6d2123cdc44
Kara <kfeilich@gmail.com>
2017-02-09 17:25:27 -0500

    Updated to calculate tailbeat amplitude and period, and plot these against streamwise velocity and acceleration. Also has code for FFT of freq, but commented out.

--- process_points_pandas.py
+++ process_points_pandas.py
@@ -111,7 +111,6 @@ for trial in tracklist:# Iterates over all avalable trials
     m = cm.ScalarMappable(cmap = cm.cubehelix)
     m.set_array(tracklist[trial]['data'].index.values)
     plt.colorbar(m, shrink=0.5, aspect=10)
-    #plt.show()
 
     #Raw Plot Pt 2, 3D
     ax = fig.add_subplot(4,2,2, projection='3d')
@@ -125,7 +124,7 @@ for trial in tracklist:# Iterates over all avalable trials
     ax.set_ylabel('Y position')
     ax.set_zlabel('Z position')
     plt.colorbar(m, shrink=0.5, aspect=10)
-    #plt.show()
+
 
     #Smoothed Data Pt 1, 3D
     ax = fig.add_subplot(4, 2, 3, projection='3d')
@@ -139,7 +138,7 @@ for trial in tracklist:# Iterates over all avalable trials
     ax.set_ylabel('Y position')
     ax.set_zlabel('Z position')
     plt.colorbar(m, shrink=0.5, aspect=10)
-    #plt.show()
+
 
     #Smoothed Data Pt 2, 3D
     ax = fig.add_subplot(4,2,4, projection='3d')
@@ -153,28 +152,28 @@ for trial in tracklist:# Iterates over all avalable trials
     ax.set_ylabel('Y position')
     ax.set_zlabel('Z position')
     plt.colorbar(m, shrink=0.5, aspect=10)
-    #plt.show()
+
 
     #Streamwise Velocity Pt 1
     ax = fig.add_subplot(4,2,5)
     ax.set_title('Pt 1 Streamwise Velocity')
     plt.scatter(x = tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt1x_v_smth'], c=colors, edgecolor='none')
     plt.colorbar(m, shrink=0.5, aspect=10)
-    #plt.show()
+
 
     #Streamwise Velocity Pt 2
     ax = fig.add_subplot(4,2,6)
     ax.set_title('Pt 2 Streamwise Velocity')
     plt.scatter(x = tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt2x_v_smth'], c=colors, edgecolor='none')
     plt.colorbar(m, shrink=0.5, aspect=10)
-    #plt.show()
+
 
     # Streamwise Accel Pt 1
     ax = fig.add_subplot(4, 2, 7)
     ax.set_title('Pt 1 Streamwise Acceleration')
     plt.scatter(x=tracklist[trial]['data'].index.values, y=-tracklist[trial]['data']['pt1x_a'], c=colors, edgecolor='none')
     plt.colorbar(m, shrink=0.5, aspect=10)
-    # plt.show()
+
 
     # Streamwise Accel Pt 2
     ax = fig.add_subplot(4, 2, 8)
@@ -187,7 +186,12 @@ for trial in tracklist:# Iterates over all avalable trials
 ######################################################################################################################
 ##Finding Tail Beat Frequency and Amplitude
 ######################################################################################################################
+tail_amplitudes = {}
+tail_periods = {}
 for trial in tracklist:# Iterates over all avalable trials
+    trial_name = tracklist[trial]['sequence']
+    fish = tracklist[trial]['fish']
+
     # By FFT
     time = tracklist[trial]['data'].index.values
     N = time.size
@@ -203,30 +207,87 @@ for trial in tracklist:# Iterates over all avalable trials
     #plt.show()
 
     # By fin beats
-    tailtip = tracklist[trial]['data']['pt2y_smth']
+    tailtip = tracklist[trial]['data']['pt2y_smth'] # get tailbeat position
+    peakindexes = peakutils.indexes(tailtip, thres=0.05, min_dist=1) # find finbeat peaks
+    troughindexes = peakutils.indexes(-tailtip, thres=0.05, min_dist=1) # fins finbeat troughs
 
-    peakindexes = peakutils.indexes(tailtip, thres=0.05, min_dist=1)
-    troughindexes = peakutils.indexes(-tailtip, thres=0.05, min_dist=1)
+    peak_times = time[peakindexes] # find when the peaks are
+    trough_times = time[troughindexes] # find when the troughs are
 
-    peak_times = time[peakindexes]
-    trough_times = time[troughindexes]
+    peak_positions = tailtip[peak_times] # find where fin is at peak
+    trough_positions = tailtip[trough_times] # find where fin is at trough
 
-    peak_positions = tailtip[peak_times]
-    trough_positions = tailtip[trough_times]
+    N_peaks = np.arange(len(peak_times)) # count peaks
+    N_troughs = np.arange(len(trough_times)) # count troughs
 
-    if peak_times[0] < trough_times[0] and len(peak_times) == len(trough_times):
-        finbeat_amplitude = peak_positions - trough_positions
-        finbeat_period = trough_times - peak_times
+    isfirst_peaks = peak_times[0] < trough_times[0] #figure out where you start
+    N_both = np.intersect1d(N_peaks, N_troughs) # find out how many finbeats you have both for
+    N_bothdiff = np.delete(N_both, len(N_both) - 1)
 
-    elif  peak_times[0] < trough_times[0] and len(peak_times) > len(trough_times):
-        finbeat_amplitude = peak_positions[:,len(peak_positions)-1] - trough_positions
-        finbeat_period = trough_times - peak_times[:,len(peak_times)-1]
+    fb_peaks = peak_positions.values[N_both] # find peaks you have assoc. with troughs
+    fb_troughs = trough_positions.values[N_both] #find troughs you have associated with peaks
 
-    elif peak_times[0] < trough_times[0] and len(peak_times) == len(trough_times):
-        finbeat_amplitude = abs(trough_positions - peak_positions)
-        finbeat_period = peak_times - trough_times
+    # Calculate fin excursion
+    if isfirst_peaks == True:
+        fb_amp = np.subtract(fb_peaks[N_both], fb_troughs[N_both])
+        beat_times = peak_times[N_both]
     else:
-        finbeat_amplitude = abs(trough_positions[:,len(trough_positions)-1] - peak_positions)
-        finbeat_period = trough_times[:, len(trough_times) - 1] - peak_times
+        fb_amp = abs(np.subtract(fb_troughs[N_both], fb_peaks[N_both]))
+        beat_times = trough_times[N_both]
 
-    finbeat_index = range(len(finbeat_period))
+    # Calculate period of each finbeat
+    fb_period = np.ediff1d(peak_times[N_both])
+
+    tail_amplitudes[trial_name] = {'sequence': trial_name, 'fish': fish, 'peak_times': peak_times,
+                                   'trough_times': trough_times, 'finbeat_number': len(N_both), 'fin_excursion': fb_amp}
+    tail_periods[trial_name] = {'sequence': trial_name, 'fish': fish}
+
+    # plot finbeat parameters against acceleration
+    fig = plt.figure()
+    fig.suptitle(tracklist[trial]['sequence'])
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax1.plot(beat_times, fb_amp, 'bo')
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Tail Excursion (px)', color='b')
+    ax1.tick_params('y', colors='b')
+    ax2 = ax1.twinx()
+    ax2.plot(tracklist[trial]['data'].index.values, -tracklist[trial]['data']['pt1x_a'], 'r.')
+    ax2.set_ylabel('Streamwise accel (px/s2)', color='r')
+    ax2.tick_params('y', colors='r')
+
+    ax3 = fig.add_subplot(2, 1, 2)
+    ax3.plot(beat_times[N_bothdiff], fb_period, 'bo')
+    ax3.set_xlabel('Time (s)')
+    ax3.set_ylabel('Tailbeat Period (s)', color='b')
+    ax3.tick_params('y', colors='b')
+
+    ax4 = ax3.twinx()
+    ax4.plot(tracklist[trial]['data'].index.values, -tracklist[trial]['data']['pt1x_a'], 'r.')
+    ax4.set_ylabel('Streamwise accel (px/s2)', color='r')
+    ax4.tick_params('y', colors='r')
+    plt.show()
+
+    # plot finbeat parameters against velocity
+    fig = plt.figure()
+    fig.suptitle(tracklist[trial]['sequence'])
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax1.plot(beat_times, fb_amp, 'bo')
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Tail Excursion (px)', color='b')
+    ax1.tick_params('y', colors='b')
+    ax2 = ax1.twinx()
+    ax2.plot(tracklist[trial]['data'].index.values, -tracklist[trial]['data']['pt1x_v'], 'r.')
+    ax2.set_ylabel('Streamwise Velocity (px/s)', color='r')
+    ax2.tick_params('y', colors='r')
+
+    ax3 = fig.add_subplot(2, 1, 2)
+    ax3.plot(beat_times[N_bothdiff], fb_period, 'bo')
+    ax3.set_xlabel('Time (s)')
+    ax3.set_ylabel('Tailbeat Period (s)', color='b')
+    ax3.tick_params('y', colors='b')
+
+    ax4 = ax3.twinx()
+    ax4.plot(tracklist[trial]['data'].index.values, -tracklist[trial]['data']['pt1x_v'], 'r.')
+    ax4.set_ylabel('Streamwise Velocity(px/s)', color='r')
+    ax4.tick_params('y', colors='r')
+    plt.show()
