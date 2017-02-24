ce88721a8e5a14062fb60b0c1957768350047e9b
Kara <kfeilich@gmail.com>
2017-02-24 13:09:07 -0500

    Trying a different peak finding algorithm, as peak_utils seems broken. Note, this one doesn't work either. Also, plotting has been commented out.

--- plot_finbeats.py
+++ plot_finbeats.py
@@ -19,7 +19,9 @@ def plot_finbeats(tracklist_subset, tracklist, finbeat_params):
 
         fig = plt.figure()
         fig.suptitle(tracklist[trial]['sequence'])
-        ax1 = fig.add_subplot(2, 1, 1)
+
+        # Subplot 1: Effort and X-Acceleration on Time
+        ax1 = fig.add_subplot(3, 2, 1)
         ax1.plot(fb_peaktimes, fb_effort, 'bo')
         ax1.set_xlabel('Time (s)')
         ax1.set_ylabel('Tailbeat Effort (cm/s)', color='b')
@@ -30,7 +32,8 @@ def plot_finbeats(tracklist_subset, tracklist, finbeat_params):
         ax2.set_ylabel('Streamwise accel (cm/s2)', color='r')
         ax2.tick_params('y', colors='r')
 
-        ax3 = fig.add_subplot(2, 1, 2)
+        # Sublplot 2: Effort and X-Velocity on Time
+        ax3 = fig.add_subplot(3, 2, 2)
         ax3.plot(fb_peaktimes, fb_effort, 'bo')
         ax3.set_xlabel('Time (s)')
         ax3.set_ylabel('Tailbeat Effort (cm/s)', color='b')
@@ -40,4 +43,52 @@ def plot_finbeats(tracklist_subset, tracklist, finbeat_params):
                  -tracklist[trial]['data']['pt1x_v'], 'r.')
         ax4.set_ylabel('Streamwise Velocity(cm/s)', color='r')
         ax4.tick_params('y', colors='r')
+
+        # Subplot 3: Amplitude and X-Acceleration on Time
+        ax5 = fig.add_subplot(3, 2, 3)
+        ax5.plot(fb_peaktimes, fb_amplitude, 'bo')
+        ax5.set_xlabel('Time (s)')
+        ax5.set_ylabel('Tailbeat Amplitude (cm)', color='b')
+        ax5.tick_params('y', colors='b')
+        ax6 = ax5.twinx()
+        ax6.plot(tracklist[trial]['data'].index.values,
+                 -tracklist[trial]['data']['pt1x_a'], 'r.')
+        ax6.set_ylabel('Streamwise accel (cm/s2)', color='r')
+        ax6.tick_params('y', colors='r')
+
+        # Subplot 4: Amplitude and X-Velocity on Time
+        ax7 = fig.add_subplot(3, 2, 4)
+        ax7.plot(fb_peaktimes, fb_amplitude, 'bo')
+        ax7.set_xlabel('Time (s)')
+        ax7.set_ylabel('Tailbeat Amplitude (cm)', color='b')
+        ax7.tick_params('y', colors='b')
+        ax8 = ax7.twinx()
+        ax8.plot(tracklist[trial]['data'].index.values,
+                 -tracklist[trial]['data']['pt1x_v'], 'r.')
+        ax8.set_ylabel('Streamwise Velocity(cm/s)', color='r')
+        ax8.tick_params('y', colors='r')
+
+        # Subplot 5: Period and X-Acceleration on Time
+        ax9 = fig.add_subplot(3, 2, 5)
+        ax9.plot(fb_peaktimes, fb_period, 'bo')
+        ax9.set_xlabel('Time (s)')
+        ax9.set_ylabel('Tailbeat Period (s)', color='b')
+        ax9.tick_params('y', colors='b')
+        ax10 = ax9.twinx()
+        ax10.plot(tracklist[trial]['data'].index.values,
+                 -tracklist[trial]['data']['pt1x_a'], 'r.')
+        ax10.set_ylabel('Streamwise accel (cm/s2)', color='r')
+        ax10.tick_params('y', colors='r')
+
+        # Subplot 6: Period and X-Velocity on Time
+        ax11 = fig.add_subplot(3, 2, 6)
+        ax11.plot(fb_peaktimes, fb_period, 'bo')
+        ax11.set_xlabel('Time (s)')
+        ax11.set_ylabel('Tailbeat Period (s)', color='b')
+        ax11.tick_params('y', colors='b')
+        ax12 = ax11.twinx()
+        ax12.plot(tracklist[trial]['data'].index.values,
+                 -tracklist[trial]['data']['pt1x_v'], 'r.')
+        ax12.set_ylabel('Streamwise Velocity(cm/s)', color='r')
+        ax12.tick_params('y', colors='r')
         plt.show()
--- plot_track.py
+++ plot_track.py
@@ -40,111 +40,117 @@ def plot_tracks(tracklist_subset, tracklist):
         scaled_time = (tracklist[trial]['data'].index.values -
                        tracklist[trial]['data'].index.values.min()) / \
                       tracklist[trial]['data'].index.values.ptp()
+
+        # Determining axis limits
+        pt1max_v = max(-tracklist[trial]['data']['pt1x_v_smth'])
+        pt1min_v = min(-tracklist[trial]['data']['pt1x_v_smth'])
+        pt1max_a = max(-tracklist[trial]['data']['pt1x_a'])
+        pt1min_a = min(-tracklist[trial]['data']['pt1x_a'])
+        pt1_vbuff = 0.05*pt1max_v
+        pt1_abuff = 0.05*pt1max_a
+
+        pt2max_v = max(-tracklist[trial]['data']['pt2x_v_smth'])
+        pt2min_v = min(-tracklist[trial]['data']['pt2x_v_smth'])
+        pt2max_a = max(-tracklist[trial]['data']['pt2x_a'])
+        pt2min_a = min(-tracklist[trial]['data']['pt2x_a'])
+        pt2_vbuff = 0.05 * pt2max_v
+        pt2_abuff = 0.05 * pt2max_a
+
         # Pull from colormap (here cubehelix)
         colors = plt.cm.cubehelix(scaled_time)
 
         # Raw Plot Pt 1, 3D
         fig = plt.figure()
+        fig.set_figheight(20)
+        fig.set_figwidth(15)
         fig.suptitle(tracklist[trial]['sequence'])
-        ax = fig.add_subplot(4, 2, 1, projection='3d')
-        ax.set_title('Pt 1 Raw Position')
-        p = ax.scatter3D(xs=tracklist[trial]['data']['pt1x'],
+        ax1 = fig.add_subplot(4, 2, 1, projection='3d')
+        ax1.set_title('Pt 1 Raw Position')
+        ax1.scatter3D(xs=tracklist[trial]['data']['pt1x'],
                          ys=tracklist[trial]['data']['pt1y'],
                          zs=tracklist[trial]['data']['pt1z'],
                          zdir='z', s=3, c=colors, marker='o',
                          edgecolor='none')  # 3D Scatter plot
-        # ax.set_xlim3d(0,1000)
-        # ax.set_ylim3d(0,1000)
-        # ax.set_zlim3d(0,1000)
-        ax.autoscale(enable=True, tight=True)
-        ax.set_xlabel('X position')
-        ax.set_ylabel('Y position')
-        ax.set_zlabel('Z position')
+        ax1.autoscale(enable=True, tight=True)
+        ax1.set_xlabel('X position')
+        ax1.set_ylabel('Y position')
+        ax1.set_zlabel('Z position')
         m = cm.ScalarMappable(cmap=cm.cubehelix)
         m.set_array(tracklist[trial]['data'].index.values)
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Raw Plot Pt 2, 3D
-        ax = fig.add_subplot(4, 2, 2, projection='3d')
-        ax.set_title('Pt 2 Raw Position')
-        p2 = ax.scatter3D(xs=tracklist[trial]['data']['pt2x'],
+        ax2 = fig.add_subplot(4, 2, 2, projection='3d')
+        ax2.set_title('Pt 2 Raw Position')
+        ax2.scatter3D(xs=tracklist[trial]['data']['pt2x'],
                           ys=tracklist[trial]['data']['pt2y'],
                           zs=tracklist[trial]['data']['pt2z'],
                           zdir='z', s=3, c=colors, marker='o',
                           edgecolor='none')  # 3D Scatter plot
-        # ax2.set_xlim3d(0,1000)
-        # ax2.set_ylim3d(0,1000)
-        # ax2.set_zlim3d(0,1000)
-        ax.autoscale(enable=True, tight=True)
-        ax.set_xlabel('X position')
-        ax.set_ylabel('Y position')
-        ax.set_zlabel('Z position')
+        ax2.autoscale(enable=True, tight=True)
+        ax2.set_xlabel('X position')
+        ax2.set_ylabel('Y position')
+        ax2.set_zlabel('Z position')
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Smoothed Data Pt 1, 3D
-        ax = fig.add_subplot(4, 2, 3, projection='3d')
-        ax.set_title('Pt 1 Smoothed Position')
-        p3 = ax.scatter3D(xs=tracklist[trial]['data']['pt1x_smth'],
+        ax3 = fig.add_subplot(4, 2, 3, projection='3d')
+        ax3.set_title('Pt 1 Smoothed Position')
+        ax3.scatter3D(xs=tracklist[trial]['data']['pt1x_smth'],
                           ys=tracklist[trial]['data']['pt1y_smth'],
                           zs=tracklist[trial]['data']['pt1z_smth'],
                           zdir='z', s=3, c=colors, marker='o',
                           edgecolor='none')  # Scatter plot
-        # ax3.set_xlim3d(0,1000)
-        # ax3.set_ylim3d(0,1000)
-        # ax3.set_zlim3d(0,1000)
-        ax.autoscale(enable=True, tight=True)
-        ax.set_xlabel('X position')
-        ax.set_ylabel('Y position')
-        ax.set_zlabel('Z position')
+        ax3.autoscale(enable=True, tight=True)
+        ax3.set_xlabel('X position')
+        ax3.set_ylabel('Y position')
+        ax3.set_zlabel('Z position')
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Smoothed Data Pt 2, 3D
-        ax = fig.add_subplot(4, 2, 4, projection='3d')
-        ax.set_title('Pt 2 Smoothed Position')
-        p4 = ax.scatter3D(xs=tracklist[trial]['data']['pt2x_smth'],
+        ax4 = fig.add_subplot(4, 2, 4, projection='3d')
+        ax4.set_title('Pt 2 Smoothed Position')
+        ax4.scatter3D(xs=tracklist[trial]['data']['pt2x_smth'],
                           ys=tracklist[trial]['data']['pt2y_smth'],
                           zs=tracklist[trial]['data']['pt2z_smth'],
                           zdir='z', s=3, c=colors, marker='o',
                           edgecolor='none')  # Scatter plot
-        # #ax4.set_xlim3d(0,1000)
-        # #ax4.set_ylim3d(0,1000)
-        # #ax4.set_zlim3d(0,1000)
-        ax.autoscale(enable=True, tight=True)
-        ax.set_xlabel('X position')
-        ax.set_ylabel('Y position')
-        ax.set_zlabel('Z position')
+        ax4.autoscale(enable=True, tight=True)
+        ax4.set_xlabel('X position')
+        ax4.set_ylabel('Y position')
+        ax4.set_zlabel('Z position')
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Streamwise Velocity Pt 1
-        ax = fig.add_subplot(4, 2, 5)
-        ax.set_title('Pt 1 Streamwise Velocity')
+        ax5 = fig.add_subplot(4, 2, 5)
+        ax5.set_title('Pt 1 Streamwise Velocity')
         plt.scatter(x=tracklist[trial]['data'].index.values,
                     y=-tracklist[trial]['data']['pt1x_v_smth'],
                     c=colors, edgecolor='none')
+        ax5.set_ylim([pt1min_v - pt1_vbuff, pt1max_v+pt1_vbuff])
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Streamwise Velocity Pt 2
-        ax = fig.add_subplot(4, 2, 6)
-        ax.set_title('Pt 2 Streamwise Velocity')
+        ax6 = fig.add_subplot(4, 2, 6)
+        ax6.set_title('Pt 2 Streamwise Velocity')
         plt.scatter(x=tracklist[trial]['data'].index.values,
                     y=-tracklist[trial]['data']['pt2x_v_smth'],
                     c=colors, edgecolor='none')
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Streamwise Accel Pt 1
-        ax = fig.add_subplot(4, 2, 7)
-        ax.set_title('Pt 1 Streamwise Acceleration')
+        ax7 = fig.add_subplot(4, 2, 7)
+        ax7.set_title('Pt 1 Streamwise Acceleration')
         plt.scatter(x=tracklist[trial]['data'].index.values,
                     y=-tracklist[trial]['data']['pt1x_a'],
                     c=colors, edgecolor='none')
         plt.colorbar(m, shrink=0.5, aspect=10)
 
         # Streamwise Accel Pt 2
-        ax = fig.add_subplot(4, 2, 8)
-        ax.set_title('Pt 2 Streamwise Acceleration')
+        ax8 = fig.add_subplot(4, 2, 8)
+        ax8.set_title('Pt 2 Streamwise Acceleration')
         plt.scatter(x=tracklist[trial]['data'].index.values,
                     y=-tracklist[trial]['data']['pt2x_a'],
                     c=colors, edgecolor='none')
         plt.colorbar(m, shrink=0.5, aspect=10)
         plt.show()
-    '''
