Fixed the original peak finding algorithm -- problem had to do with indices of inputs. Started working on a finbeat calculation function for amplitude, period, and effort calculations

process_points_pandas.py
@@ -44,7 +44,7 @@ for filename in os.listdir(folder):  # For all files in the directory
         fish = file_info[0]
         sequence = file_info[1]
         trial_name = fish+sequence
-        framerate = trial_info['FPS'][trial_name]
+        framerate = float(trial_info['FPS'][trial_name])
         L_calib = trial_info['ScaleL_cm/px'][trial_name]
         V_calib = trial_info['ScaleV_cm/px'][trial_name]
         init_Speed = trial_info['InitialSpd_cm'][trial_name]
@@ -135,7 +135,8 @@ for filename in os.listdir(folder):  # For all files in the directory
         
         # Put all of these into the appropriate object in tracklist
         tracklist[trial_name] = {'sequence': trial_name, 'fish': fish,
-                                 'fish_TL': fish_TL, 'behavior': behavior,
+                                 'fish_TL': fish_TL, 'FPS': framerate,
+                                 'behavior': behavior,
                                  'data': df}
         # Advance the count
         count = count + 1
@@ -257,12 +258,14 @@ for filename in os.listdir(folder):  # For all files in the directory
     plt.colorbar(m, shrink=0.5, aspect=10)
     plt.show()
 '''
+# Can use plot_track and plot_accel for same functionality
 
 ########################################################################
 # Define peak detection function
 ########################################################################
-# Detect peaks in data based on  amplitude + other features. by Marcos Duarte
-__author__ = "Marcos Duarte, https://github.com/demotu/BMC"
+# Detect peaks in data based on  amplitude + other features.
+# by Marcos Duarte
+'''__author__ = "Marcos Duarte, https://github.com/demotu/BMC"
 __version__ = "1.0.4"
 __license__ = "MIT"
 
@@ -402,12 +405,12 @@ def _plot(x, mph, mpd, threshold, edge, valley, ax, ind):
         ax.set_title("%s (mph=%s, mpd=%d, threshold=%s, edge='%s')"
                      % (mode, str(mph), mpd, str(threshold), edge))
         # plt.grid()
-        plt.show()
+        plt.show()'''
 
 ########################################################################
 # Define baseline detection function
 ########################################################################
-# Detect baseline trend in cyclical data
+'''# Detect baseline trend in cyclical data
 __author__ = "Kara Feilich"
 __version__ = "1.0.0"
 
@@ -430,13 +433,14 @@ def find_baseline(x,y):
     fitslope, intercept, r_value, p_value, std_err = scipy.stats.linregress(
         x, y)
     baseline = np.array(fitslope * x + intercept)
-    return baseline
+    return baseline'''
 
 ########################################################################
 # Finding Tail Beat Frequency and Amplitude
 ########################################################################
 tail_amplitudes = {}
 tail_periods = {}
+'''
 for trial in tracklist:  # Iterates over all avalable trials
     trial_name = tracklist[trial]['sequence']
     fish = tracklist[trial]['fish']
@@ -494,7 +498,7 @@ for trial in tracklist:  # Iterates over all avalable trials
                                    'fin_excursion': fb_amp}
     tail_periods[trial_name] = {'sequence': trial_name, 'fish': fish,
                                 'fin_periods': fb_period}
-    '''
+
     # plot finbeat parameters against acceleration
     fig = plt.figure()
     fig.suptitle(tracklist[trial]['sequence'])
@@ -552,7 +556,7 @@ for trial in tracklist:  # Iterates over all avalable trials
 ########################################################################
 # Calculating finbeat effort
 ########################################################################
-finbeat_params = {}
+'''finbeat_params = {}
 for trial in tracklist:  # Iterates over all avalable trials
     trial_name = tracklist[trial]['sequence']
     fish = tracklist[trial]['fish']
@@ -570,7 +574,7 @@ for trial in tracklist:  # Iterates over all avalable trials
                                   'finbeat_periods': fb_periods,
                                   'finbeat_effort': fb_effort}
 
-    '''fig = plt.figure()
+    fig = plt.figure()
     fig.suptitle(tracklist[trial]['sequence'])
     ax1 = fig.add_subplot(2, 1, 1)
     ax1.plot(fb_peaktimes, fb_effort, 'bo')