44b4a32ad9c36e243fe7fd6e3d95981b74523d58
Kara <kfeilich@gmail.com>
2017-03-07 18:49:45 -0500

    Replaced plot_net_accel with check_plots to quickly check if the net values and the y-axis values for velocity and acceleration seem sensible

--- process_points.py
+++ process_points.py
@@ -2,6 +2,7 @@ from extract_data import extract_data
 from finbeat_calc import finbeat_calc
 from plot_accel import plot_accel
 from plot_track import plot_track
+from check_plots import check_plots
 
 # Take position data from hardcoded folder, calc. velocities, accels
 tracklist = extract_data()
@@ -15,7 +16,7 @@ finbeats = finbeat_calc(tracklist.keys(), tracklist)
 # Plot things if desired
 # plot_track(['sometrial'], tracklist)
 # plot_accel(['sometrial'], tracklist)
-# plot_net_accel(['sometrial'], tracklist)
+# check_plots(['sometrial'], tracklist)
 
 # """If you want to plot the peaks and troughs on the position data,
 # uncomment section in finbeat_calc"""
