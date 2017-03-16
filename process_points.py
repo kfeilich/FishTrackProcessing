c6b49f47f3cbba76d1436e6372d7bae1b439b796
Kara <kfeilich@gmail.com>
2017-03-16 03:05:28 -0400

    Added (and commented out) make_subset() and plot_analysis() functions in their appropriate places in process_points, to make order of use of each function clear.

--- process_points.py
+++ process_points.py
@@ -1,23 +1,25 @@
 from extract_data import extract_data
 from finbeat_calc import finbeat_calc
+from make_subset import make_subset
 from plot_accel import plot_accel
 from plot_track import plot_track
 from check_plots import check_plots
+from plot_analysis import plot_analysis
 
 # Take position data from hardcoded folder, calc. velocities, accels
 tracklist = extract_data()
 
 # Calculate finbeat peaks and troughs
-
+# Extract finbeat periods, amplitudes, and subsequent accel
 finbeats, finbeat_byP, finbeat_byT = finbeat_calc(tracklist.keys(),
                                              tracklist)
 
-# Extract finbeat periods, amplitudes, and subsequent accel
-
 # Plot things if desired
+# make_subset(group_by, identifier, tracklist)
 # plot_track(['sometrial'], tracklist)
 # plot_accel(['sometrial'], tracklist)
 # check_plots(['sometrial'], tracklist)
+# plot_analysis('subset_name'),['sometrial'], finbeat_data, tracklist)
 
 # """If you want to plot the peaks and troughs on the position data,
 # uncomment section in finbeat_calc"""
