4f8a97fc3214c1497ffae33fa4cdf08fca76faa6
Kara <kfeilich@gmail.com>
2017-03-29 11:01:48 -0400

    speed_heatmap: function to calc pairwise differences in trial speeds

--- speed_heatmap.py
+++ speed_heatmap.py
@@ -9,7 +9,7 @@ def speed_heatmap(subset, tracklist):
         for j in subset:
             speed2 = tracklist[j]['start_spd']
 
-            speed_diff = np.absolute(np.subtract(speed-speed2))
+            speed_diff = np.absolute(np.subtract(speed, speed2))
             speed_diff_mat[i][j] = speed_diff
 
     speed_diff_mat = speed_diff_mat.apply(pd.to_numeric)
